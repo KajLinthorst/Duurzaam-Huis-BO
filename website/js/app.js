462468dee7218aaa435dc5e4ffb80e8f83ce3a0d
KajLinthorst <37048@ma-web.nl>
2024-04-09 14:03:23 +0200

    stone roses on top

---
+++ website/js/app.js
@@ -0,0 +1,8 @@
+console.log("Hallo wereld wereld")
+
+let weerImg = document.getElementById("js--weatherImage");
+let weer = "thunder"; // Soorten weer zijn "sun", "rain", "snow", "thunder", "cloud"
+
+
+weerImg.src = ("img/weather/" + weer + ".png");
+
